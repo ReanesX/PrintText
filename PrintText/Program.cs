d92aaac5ebed9de9d0ba3ba0c4e33004c3922c24
ReanesX <miha200273@gmail.com>
2020-10-02 19:57:03 +0500

    Добавьте файлы проекта.

---
+++ PrintText/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace PrintText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.Write("Введите текст: ");
+                string text = Console.ReadLine();
+
+                char[] charText = text.ToCharArray();
+
+                Console.Write("Введите кол-во задержки: ");
+                int timeout = Convert.ToInt32(Console.ReadLine());
+
+                Console.Clear();
+
+                for (int i = 0; i < charText.Length; i++)
+                {
+                    Console.Write(charText[i]);
+                    Thread.Sleep(timeout);
+                }
+                
+                Console.WriteLine("\nПовторить Y/N ?");
+                if (Console.ReadKey(true).Key != ConsoleKey.Y)
+                    break;
+            }
+        }
+    }
+}
